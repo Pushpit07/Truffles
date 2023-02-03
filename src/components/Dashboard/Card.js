5a6cb2a5d1dc2f0a879f72e92a755dcad0447dec
Pushpit <pushpit07@gmail.com>
2023-02-04 00:46:00 +0530

    Added animation to card balances
    Signed-off-by: Pushpit <pushpit07@gmail.com>

--- src/components/Dashboard/Card.js
+++ src/components/Dashboard/Card.js
@@ -1,17 +1,48 @@
+import { useEffect, useState } from "react";
 import Image from "next/image";
+import { motion, animate } from "framer-motion";
 
 export default function Card({ imgSrc, text, balance }) {
+	const [rounded, setRounded] = useState(99999);
+
+	function padZeros(n, width) {
+		n = n + "";
+		return n.length >= width ? n : new Array(width - n.length + 1).join("0") + n;
+	}
+
+	useEffect(() => {
+		const animation = animate(99999, balance, {
+			duration: 3,
+			onUpdate: (latest) => {
+				setRounded(padZeros(Math.round(latest), 5).replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+			},
+		});
+		return animation.stop;
+	}, [balance]);
+
 	return (
-		<div className="flex basis-1/2 justify-between p-8 bg-light-100 rounded-2xl shadow-xl shadow-black/[.05]">
+		<motion.div
+			className="flex basis-1/2 justify-between p-8 bg-light-100 rounded-2xl shadow-xl shadow-black/[.05]"
+			initial={{ opacity: 0, scale: 0.5 }}
+			animate={{ opacity: 1, scale: 1 }}
+			transition={{
+				duration: 0.8,
+				ease: [0, 0.7, 0.2, 1.01],
+			}}
+			// initial={{ opacity: 0 }}
+			// animate={{ opacity: 1 }}
+		>
 			<div className="flex flex-col gap-y-4">
 				<div className="text-dark-200 text-base">{text}</div>
-				<div className="text-dark-500 font-bold text-2xl">{balance}</div>
+				<div className="flex text-dark-500 font-bold text-2xl">
+					$<motion.h1>{rounded.toLocaleString("en-US")}</motion.h1>
+				</div>
 			</div>
 			<div>
 				<div className="flex justify-center itmes-center p-4 rounded-lg bgGradient shadow-md">
 					<Image src={imgSrc} width="30" height="30" alt="Dashboard" />
 				</div>
 			</div>
-		</div>
+		</motion.div>
 	);
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import SidebarButton from "@/components/Dashboard/SidebarButton";
 import Card from "@/components/Dashboard/Card";
 import Chart from "chart.js/auto";
+import { motion } from "framer-motion";
 
 export default function HomePage() {
 	const canvasEl = useRef(null);
@@ -136,14 +137,14 @@ export default function HomePage() {
 			<main className="flex flex-col items-center justify-center w-full bg-light-200">
 				<div className="w-full max-w-[1920px] flex justify-center items-start min-h-screen">
 					<div className="relative w-full flex flex-row pr-6">
-						<div className="basis-1/5 p-10 space-y-4">
+						<motion.div className="basis-1/5 p-10 space-y-4">
 							<SidebarButton imgSrc="/dashboard/dashboard.png" text="Dashboard" active={true} />
 							<SidebarButton imgSrc="/dashboard/invoice.png" text="Invoice" />
 							<SidebarButton imgSrc="/dashboard/proforma.png" text="Proforma" />
 							<SidebarButton imgSrc="/dashboard/swap.png" text="Swap" />
 							<SidebarButton imgSrc="/dashboard/transfer.png" text="Transfer" />
 							<SidebarButton imgSrc="/dashboard/contacts.png" text="Contacts" />
-						</div>
+						</motion.div>
 
 						<div className="basis-4/5 py-10 gap-y-4">
 							<div className="bg-primary-500 rounded-lg py-5 px-6">
@@ -164,10 +165,10 @@ export default function HomePage() {
 
 							<div className="w-full flex flex-row justify-between items-start gap-x-5 mt-6">
 								<div className="w-4/6 grid grid-cols-2 gap-5 justify-between items-center">
-									<Card imgSrc="/dashboard/fiat.png" text="Fiat Balance" balance="$00,000" />
-									<Card imgSrc="/dashboard/crypto_balance.png" text="Crypto Balance" balance="$00,000" />
-									<Card imgSrc="/dashboard/receivable.png" text="Receivable" balance="$00,000" />
-									<Card imgSrc="/dashboard/payables.png" text="Payables" balance="$00,000" />
+									<Card imgSrc="/dashboard/fiat.png" text="Fiat Balance" balance={0} />
+									<Card imgSrc="/dashboard/crypto_balance.png" text="Crypto Balance" balance={0} />
+									<Card imgSrc="/dashboard/receivable.png" text="Receivable" balance={0} />
+									<Card imgSrc="/dashboard/payables.png" text="Payables" balance={0} />
 
 									<div className="col-span-2 p-8 bg-light-100 shadow-xl shadow-black/[.05] rounded-xl h-full">
 										<div className="flex flex-col gap-y-4">
